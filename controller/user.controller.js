528287e59b9686d1a201023b32176df6125e0969
vinothkumarfreelancer <vinothkumar.vkfreelancer@gmail.com>
2025-07-30 18:25:37 +0530

    intern-1 updated-1

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,7 +9,7 @@ const postUser = async (req, res) => {
         if (existingemail) {
             return res.status(400).send({ message: "The email already exists...." })
         }
-        const data = await database.push(newEntry);
+        const data = database.push(newEntry);
         res.status(201).send({ message: "Data Added...", result: database })
     } catch (err) {
         res.status(500).send({ message: "Data Not Added...", result: err })
@@ -27,7 +27,11 @@ const getData = async (req, res) => {
 
 const getDataById = async (req, res) => {
     try {
-        const { id } = req.params; //10        
+        const { id } = req.params; //10 ,//undefined
+
+        if (!id || isNaN(id)) { 
+            return res.status(400).send({ message: "Invalid ID format" });
+        }
         const existingdata = database.find((ele) => {
             return ele.id === Number(id)
         })
@@ -44,16 +48,15 @@ const updateData = async (req, res) => {
     try {
         const { id } = req.params;
         const updateddata = req.body; //
-        const index = database.findIndex((ele) => {
-            return ele.id === Number(id)
-        })
-        // if (index + 1 != id) {
-        //     return res.status(400).send({ message: "Id could not found" })
-        // }
+        const index = database.findIndex((ele) => ele.id === Number(id)) // 3===4 =-1
+
+        if (index === -1) {
+            return res.status(404).send({ message: "User ID not found" });
+        }
         //  database[index]=updateddata  //patch
         database.splice(index, 1, updateddata)//0 ,1,update put
-        console.log(database);
-        
+
+
         res.status(200).send({ message: "Data updated", result: database })
 
     } catch (err) {
@@ -63,14 +66,15 @@ const updateData = async (req, res) => {
 
 const deleteData = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { id } = req.params;//vinoth 1
         const index = database.findIndex((ele) => {
             return ele.id === Number(id)
         })
-        database.splice(index, 1)//0 ,1,update
-        console.log(database);
-        
-        return  res.status(200).send({ message: "deleted successfully", result: database })
+        if (index === -1) {
+            return res.status(404).send({ message: "User ID not found" });
+        }
+        database.splice(index, 1);
+        return res.status(200).send({ message: "deleted successfully", result: database })
 
 
 
